15d1dfa5e4ce97d05ecaa33f2ac98f578486b5ed
unknown <mustaphas902@gmail.com>
2023-09-28 23:56:55 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,35 +1,9 @@
-import Header from "./components/Header";
-import ArticleOne from "./components/ArticleOne";
-import ArticleTwo from "./components/ArticleTwo";
-import ArticleThree from "./components/ArticleThree";
-import Footer from "./components/Footer";
-import ShortenLinks from "./components/ShortenLinks";
-import LinkResult from "./components/LinkResult";
-import { useState } from "react";
+import React from "react";
 
 function App() {
-  const [inputValue, setInputValue] = useState("");
   return (
-    <div className="App">
-      <Header />
-      <ArticleOne />
-      <article className="gray-section">
-        <div className="container">
-          <ShortenLinks setInputValue={setInputValue} />
-          <LinkResult inputValue={inputValue} />
-          <ArticleTwo />
-        </div>
-      </article>
-      <article className="purple-section">
-        <div className="container">
-          <ArticleThree />
-        </div>
-      </article>
-      <footer>
-        <div className="container">
-          <Footer />
-        </div>
-      </footer>
+    <div>
+
     </div>
   );
 }
